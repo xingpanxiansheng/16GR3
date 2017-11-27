c6bc06b7c0b37675eb050bf29887d933c1e24a42
xingpanxiansheng <554462590@qq.com>
2017-11-27 15:13:25 +0800

    张盼提交Struts2 T02上机作业。

---
+++ Struts2/T2/张盼/Struts2 T02/Struts2/src/com/jay/to1/action/Interceptor.java
@@ -0,0 +1,16 @@
+package com.jay.to1.action;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class Interceptor extends AbstractInterceptor {
+
+	@Override
+	public String intercept(ActionInvocation arg0) throws Exception {
+		System.out.println("我开始拦截。。。");
+		String result = arg0.invoke();
+		System.out.println("返回代码:"+result);
+		return result;
+	}
+
+}
