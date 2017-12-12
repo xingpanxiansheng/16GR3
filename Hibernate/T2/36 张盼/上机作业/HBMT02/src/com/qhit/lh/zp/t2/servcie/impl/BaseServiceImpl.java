1de76237bfd5c91a1f6a7dff7530b9f53647d46e
xingpanxiansheng <554462590@qq.com>
2017-12-12 22:05:17 +0800

    张盼提交HBT02上机作业。

---
+++ Hibernate/T2/36 张盼/上机作业/HBMT02/src/com/qhit/lh/zp/t2/servcie/impl/BaseServiceImpl.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.qhit.lh.zp.t2.servcie.impl;
+
+import java.util.List;
+
+import com.qhit.lh.zp.t2.dao.BaseDao;
+import com.qhit.lh.zp.t2.dao.impl.BaseDaoImpl;
+import com.qhit.lh.zp.t2.servcie.BaseService;
+
+
+
+/**
+ * @author 张盼
+ * 2017年12月11日
+ */
+public class BaseServiceImpl implements BaseService {
+	private BaseDao baseDao = new BaseDaoImpl();
+
+	@Override
+	public void add(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.add(obj);
+	}
+
+	@Override
+	public void delete(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.delete(obj);
+	}
+
+	@Override
+	public void update(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.update(obj);
+	}
+
+	@Override
+	public List<Object> getAll(String fromObject) {
+		// TODO Auto-generated method stub
+		return baseDao.getAll(fromObject);
+	}
+
+	@Override
+	public Object getObjectById(Object obj, int id) {
+		// TODO Auto-generated method stub
+		return baseDao.getObjectById(obj, id);
+	}
+
+}
